566686daa814726fd05734611d498ea61b606cd1
Marcos Andrade <marcospandrade2@gmail.com>
2023-05-21 20:26:55 -0300

    feat: upload file and provide static folder to get images

---
+++ server/src/routes/upload.ts
@@ -0,0 +1,53 @@
+import { randomUUID } from 'node:crypto'
+import { extname, resolve } from 'node:path'
+import { FastifyInstance } from 'fastify'
+import { createWriteStream } from 'node:fs'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
+
+const pump = promisify(pipeline)
+
+export async function uploadRoutes(app: FastifyInstance) {
+  app.post('/upload', async (request, reply) => {
+    const upload = await request.file({
+      limits: {
+        fileSize: 1024 * 1024 * 5, // 5MB
+      },
+    })
+
+    if (!upload) {
+      return reply.status(400).send()
+    }
+
+    // generate regex to validate file mimeType is image or video
+
+    // const regex = /\.(jpg|jpeg|png|gif|mp4|webm)$/
+
+    // if (!regex.test(upload.filename)) {
+    //   return reply.status(400).send()
+    // }
+
+    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
+    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+
+    if (!isValidFileFormat) {
+      return reply.status(400).send()
+    }
+
+    const fileId = randomUUID()
+    const extension = extname(upload.filename)
+
+    const fileName = `${fileId}${extension}`
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads/', fileName),
+    )
+
+    await pump(upload.file, writeStream)
+
+    const fullUrl = `${request.protocol}://${request.hostname}`
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+    return { ok: fileUrl }
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,17 +3,28 @@ import 'dotenv/config'
 import jwt from '@fastify/jwt'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
+
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
 
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(multipart)
 app.register(cors, {
   origin: true,
 })
 app.register(jwt, {
   secret: process.env.JWT_SECRET ?? 'spacetime',
 })
+
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 app.register(authRoutes)
 
